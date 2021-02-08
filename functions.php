4a4ab35aed7e6c73e4f94f2ff921d041bfbcef7a
Heiko Mamerow <mail@heikomamerow.de>
2021-02-08 23:05:49 +0100

    custom event scope

--- functions.php
+++ functions.php
@@ -14,9 +14,33 @@ add_action( 'wp_enqueue_scripts', 'astra_parent_theme_enqueue_styles' );
  */
 function astra_parent_theme_enqueue_styles() {
 	wp_enqueue_style( 'astra-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'astra-child-style',
+	wp_enqueue_style(
+		'astra-child-style',
 		get_stylesheet_directory_uri() . '/style.css',
 		array( 'astra-style' )
 	);
 
+}
+
+/**
+ * Custom event scope for Event Manager plugin.
+ *
+ * Source: https://wp-events-plugin.com/tutorials/create-your-own-event-scope/
+ */
+add_filter( 'em_events_build_sql_conditions', 'my_em_scope_conditions', 1, 2 );
+function my_em_scope_conditions( $conditions, $args ) {
+	if ( ! empty( $args['scope'] ) && $args['scope'] == 'next7days' ) {
+		$start_date          = date( 'Y-m-d', current_time( 'timestamp' ) );
+		$end_date            = date( 'Y-m-d', strtotime( '+6 day', current_time( 'timestamp' ) ) );
+		$conditions['scope'] = " (event_start_date BETWEEN CAST('$start_date' AS DATE) AND CAST('$end_date' AS DATE)) OR (event_end_date BETWEEN CAST('$end_date' AS DATE) AND CAST('$start_date' AS DATE))";
+	}
+	return $conditions;
+}
+
+add_filter( 'em_get_scopes', 'my_em_scopes', 1, 1 );
+function my_em_scopes( $scopes ) {
+	$my_scopes = array(
+		'next7days' => 'next7days',
+	);
+	return $scopes + $my_scopes;
 }
