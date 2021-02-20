29e53138c108c5e7865f5e67a38627ff74667865
Heiko Mamerow <mail@heikomamerow.de>
2021-02-20 13:36:32 +0100

    refactor(codestyle) code style testing

--- astra-child/single.php
+++ astra-child/single.php
@@ -16,23 +16,23 @@ get_header(); ?>
 
 <?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
 
-<?php get_sidebar(); ?>
+	<?php get_sidebar(); ?>
 
 <?php endif ?>
 
-<div id="primary" <?php astra_primary_class(); ?>>
+	<div id="primary" <?php astra_primary_class(); ?>>
 
-	<?php astra_primary_content_top(); ?>
+		<?php astra_primary_content_top(); ?>
 
-	<?php astra_content_loop(); ?>
+		<?php astra_content_loop(); ?>
 
-	<?php astra_primary_content_bottom(); ?>
+		<?php astra_primary_content_bottom(); ?>
 
-</div><!-- #primary -->
+	</div><!-- #primary -->
 
 <?php if ( astra_page_layout() == 'right-sidebar' ) : ?>
 
-<?php get_sidebar(); ?>
+	<?php get_sidebar(); ?>
 
 <?php endif ?>
 
