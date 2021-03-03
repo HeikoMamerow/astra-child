6b3a5b6163deaabceaac77b19b5bdf55b5252fff
Heiko Mamerow <mail@heikomamerow.de>
2021-03-03 21:06:34 +0100

    refactor(menu): complete rewritten menu logic

--- astra-child/functions.php
+++ astra-child/functions.php
@@ -54,125 +54,99 @@ add_filter( 'the_content', 'do_shortcode' );
  */
 function em_advanced_notice_func() {
 
-	/*
-	 * 1) Find all categories with events inside the scope (next7days).
-	 */
+	$string         = '';
+	$event          = [];
+	$recurrence_ids = [];
 
-	$string             = '';
-	$category_ids       = [];
-	$exclude_recurrings = [];
+	// Set to german language
+	//setlocale (LC_ALL, 'de_DE@euro', 'de_DE', 'de', 'ge');
+	setlocale( LC_ALL, 'de_DE' );
 
-
+	// Set to german time zone
 	date_default_timezone_set( 'Europe/Berlin' );
-	$scope_start       = date( 'Y-m-d' );
-	$scope_7days_later = date( 'Y-m-d', strtotime( "+1 week" ) );
-	$scope_end         = date( 'Y-m-d', strtotime( "+3 week" ) );
 
-	// ID of parent category Wochenprogramm => 15
-	$child_of = 15;
+	$scope_today        = date( 'Y-m-d' );
+	$scope_6days_later  = date( 'Y-m-d', strtotime( "+6 day" ) );
+	$scope_7days_later  = date( 'Y-m-d', strtotime( "+7 day" ) );
+	$scope_21days_later = date( 'Y-m-d', strtotime( "+21 day" ) );
 
-	$args       = [
-		'child_of'   => $child_of,
-		'type'       => 'event',
-		'hide_empty' => 1,
-		'taxonomy'   => 'event-categories',
-	];
-	$categories = get_categories( $args );
+	// Get events within the scope (today + 6 days)
+	$em_events = EM_Events::get( [
+		'hide_empty'  => 1,
+		'recurrences' => 1,
+		'orderby'     => "event_start_date,event_start_time",
+		'scope'       => $scope_today . "," . $scope_6days_later,
+	] );
 
-	foreach ( $categories as $category ) {
-
-		// Get events within the scope (3 weeks)
-		$em_events = EM_Events::get( [
-			'hide_empty'  => 1,
-			'category'    => $category->cat_ID,
-			'recurrences' => 1,
-			'orderby'     => "event_start_time",
-			'scope'       => $scope_start . "," . $scope_end,
-			//'scope'       => "next7days",
-		] );
-
-		// Get all categories within the scope.
-		foreach ( $em_events as $em_event ) {
-			if ( ! isset( $category_id ) ) {
-				$category_ids[] = $category->cat_ID;
-			};
-		}
+	// Set all event data in array.
+	foreach ( $em_events as $em_event ) {
+		$events[] = [
+			'day_number'    => date( 'N', strtotime( $em_event->start_date ) ),
+			'day'           => strftime( '%a', strtotime( $em_event->start_date ) ),
+			'timestamp'     => strtotime( $em_event->start_date ),
+			'start_time'    => date( 'G:i', strtotime( $em_event->start_time ) ),
+			'recurrence_id' => $em_event->recurrence_id,
+			'guid'          => $em_event->guid,
+			'event_name'    => $em_event->event_name,
+		];
 	}
 
-	/*
-	 * 2) Get only events from categories within the scope
-	 */
+	// Get array of recurrende_ids.
+	foreach ( $events as $event ) {
+		$recurrence_ids[] = $event['recurrence_id'];
+	}
 
-	$args       = [
-		'include'    => $category_ids,
-		'type'       => 'event',
-		'hide_empty' => 1,
-		'taxonomy'   => 'event-categories',
-	];
-	$categories = get_categories( $args );
+	// Get events within the next scope (7 - 21 days)
+	$em_events_7_to_21 = EM_Events::get( [
+		'hide_empty'  => 1,
+		'recurrences' => 1,
+		'orderby'     => "event_start_date,event_start_time",
+		'scope'       => $scope_7days_later . "," . $scope_21days_later,
+	] );
 
-	foreach ( $categories as $category ) {
+
+	foreach ( $em_events_7_to_21 as $em_event_7_to_21 ) {
+
+		// We want recurrences from the first week.
+		if ( ! in_array( $em_event_7_to_21->recurrence_id, $recurrence_ids ) ) {
+
+			// Beware we can have still multiple events from one recurrence.
+			// Add this recurrence to the check and prevent later in the loop.
+			$recurrence_ids[] = $em_event_7_to_21->recurrence_id;
+
+			$events[] = [
+				'day_number'    => date( 'N', strtotime( $em_event_7_to_21->start_date ) ),
+				'day'           => strftime( '%a', strtotime( $em_event_7_to_21->start_date ) ),
+				'timestamp'     => strtotime( $em_event_7_to_21->start_date ),
+				'start_time'    => date( 'G:i', strtotime( $em_event_7_to_21->start_time ) ),
+				'recurrence_id' => $em_event_7_to_21->recurrence_id,
+				'guid'          => $em_event_7_to_21->guid,
+				'event_name'    => $em_event_7_to_21->event_name,
+			];
+
+		}
+
+	}
+
+	// First sort by day then by time and then by timestamp.
+	$day_number = array_column( $events, 'day_number' );
+	$start_time = array_column( $events, 'start_time' );
+	$timestamp  = array_column( $events, 'timestamp' );
+	array_multisort( $day_number, SORT_ASC, $start_time, SORT_ASC, $timestamp, SORT_ASC, $events );
+
+	foreach ( $events as $event ) {
 
 		$string .= '<div class="menu-link-flex em-recurring-events-in-menu">';
-		$string .= '<div class="menu-link menu-link-day">' . $category->description . '</div>';
+		$string .= '<div class="menu-link menu-link-day">' . $event['day'] . '</div>';
 		$string .= '<div class="menu-link menu-link-event-list">';
 
-		/*
-		 * 2.a Show events from active recurrences.
-		 */
-
-		// Convert category slug (category_nicename) into date
-		$strtotime = strtotime( 'next ' . $category->category_nicename );
-		$scope     = date( 'Y-m-d', $strtotime );
-
-		// Get events within the scope (7days)
-		$em_events = EM_Events::get( [
-			'hide_empty'  => 1,
-			'category'    => $category->term_id,
-			'recurrences' => 1,
-			'orderby'     => "event_start_time",
-			'scope'       => $scope,
-		] );
-
-
-		foreach ( $em_events as $em_event ) {
-			$exclude_recurrings[] = $em_event->recurrence_id;
-
-			$string .= '<a class="menu-link-flex" href="' . $em_event->guid . '">';
-			$string .= '<span class="menu-link-flex-item2">' . date( 'G:i', strtotime( $em_event->start_time ) ) . '</span> ';
-			$string .= '<span class="menu-link-flex-item3">' . $em_event->event_name . '</span>';
-			$string .= '</a>';
-		}
-
-		/*
-		 * 2.b Show events from soon (in the next 3 weeks) starting recurrences.
-		 */
-		// Get events within the scope (3weeks)
-		$em_events = EM_Events::get( [
-			'hide_empty'  => 1,
-			'category'    => $category->cat_ID,
-			'recurrences' => 1,
-			'limit'       => 1,
-			'orderby'     => "event_start_time",
-			'scope'       => $scope_7days_later . "," . $scope_end,
-		] );
-
-		foreach ( $em_events as $em_event ) {
-
-			// Show only events not from the same recurring ID
-			if ( ! in_array( $em_event->recurrence_id, $exclude_recurrings ) ) {
-
-				$string .= '<a class="menu-link-flex" href="' . $em_event->guid . '">';
-				$string .= '<span class="menu-link-flex-item2">' . date( 'G:i', strtotime( $em_event->start_time ) ) . '</span> ';
-				$string .= '<span class="menu-link-flex-item3">' . $em_event->event_name . '</span>';
-				$string .= '</a>';
-
-			}
-		}
+		$string .= '<a class="menu-link-flex" href="' . $event['guid'] . '">';
+		$string .= '<span class="menu-link-flex-item2">' . date( 'G:i', strtotime( $event['start_time'] ) ) . '</span> ';
+		$string .= '<span class="menu-link-flex-item3">' . $event['event_name'] . '</span>';
+		$string .= '</a>';
 
 		$string .= '</div>';
 		$string .= '</div>';
-
 	}
 
 	return $string;
